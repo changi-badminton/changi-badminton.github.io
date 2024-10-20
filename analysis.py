5dfded72874a0008584e744a1e75b27a9f17529d
chiayewken <chiayewken@gmail.com>
2024-10-21 02:41:53 +0800

    Initial check_expo for sbh, not fully working yet

--- analysis.py
+++ analysis.py
@@ -1,6 +1,10 @@
 import time
+from datetime import datetime, timedelta
+from typing import List, Tuple
 
 import dotenv
+import pandas as pd
+from bs4 import BeautifulSoup
 from fire import Fire
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -45,12 +49,88 @@ def check_sutd(url: str = "https://usermgmtsys.sutd.edu.sg/login"):
     driver.quit()
 
 
+def get_week_dates(day: int, month: int, year: int) -> List[Tuple[int, int, int]]:
+    # Get current date and 6 more days
+    dates = []
+    for i in range(7):
+        date = datetime(year, month, day) + timedelta(days=i)
+        dates.append((date.day, date.month, date.year))
+    return dates
+
+
+def click_item(driver, by: str, value: str):
+    print(dict(click=by, value=value))
+    wait = WebDriverWait(driver, 10)
+    mapping = dict(css=By.CSS_SELECTOR, cls=By.CLASS_NAME, id=By.ID, xpath=By.XPATH)
+
+    try:
+        wait.until(EC.element_to_be_clickable((mapping[by], value))).click()
+    except Exception as e:
+        print(f"Could not click {value}", e)
+        breakpoint()
+
+
+def wait_item(driver, by: str, value: str):
+    print(dict(wait=by, value=value))
+    wait = WebDriverWait(driver, 10)
+    mapping = dict(css=By.CSS_SELECTOR, cls=By.CLASS_NAME, id=By.ID, xpath=By.XPATH)
+
+    try:
+        wait.until(EC.presence_of_element_located((mapping[by], value)))
+    except Exception as e:
+        print(f"Could not find {value}", e)
+        with open("error.html", "w") as f:
+            f.write(driver.page_source)
+
+    return driver.find_element(mapping[by], value)
+
+
+def check_expo(
+    url: str = "https://singaporebadmintonhall.getomnify.com/widgets/O3MRKGBH359GA55KHMG1RD",
+):
+    # Iframe from https://singaporebadmintonhall.com/expo/
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+    click_item(driver, "css", "div.dateTextWrapper")
+    element = wait_item(driver, "css", "td.xdsoft_date.xdsoft_current")
+    day = int(element.get_attribute("data-date"))
+    month = int(element.get_attribute("data-month"))
+    year = int(element.get_attribute("data-year"))
+    print(dict(day=day, month=month, year=year))
+
+    all_slots = []
+    for day, month, year in get_week_dates(day, month, year):
+        click_item(driver, "css", "div.dateTextWrapper")
+        click_item(driver, "css", "div.dateTextWrapper")
+        click_item(
+            driver,
+            "css",
+            f"td[data-date='{day}'][data-month='{month}'][data-year='{year}']",
+        )
+
+        wait_item(driver, "cls", "week-column.facility-row")
+        html = driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
+
+        for s in soup.find_all("div", class_="time-slot facility-slot"):
+            raw = s.attrs
+            print(raw)
+            if "arina" in raw.get("data-facility_name", "").lower():
+                continue
+            all_slots.append(dict(day=day, month=month + 1, year=year, **raw))
+
+        df = pd.DataFrame(all_slots)
+        print(df)
+
+
 """
 p analysis.py take_screenshot --url "https://www.carc.org.sg/FacilityBooking.aspx"
 p analysis.py take_screenshot --url "http://ibms.sutd.edu.sg"
 p analysis.py take_screenshot --url "https://usermgmtsys.sutd.edu.sg/login"
 p analysis.py take_screenshot --url "https://usermgmtsys.sutd.edu.sg/login"
 python analysis.py check_sutd
+python analysis.py check_expo
 """
 
 if __name__ == "__main__":
